86832ea609d5cb6e92d7ab0760701c58ff2fb53a
Timm Friebe <thekid@thekid.de>
2014-12-27 14:17:46 +0100

    Add test adding files and a directory

--- src/test/php/io/archive/zip/unittest/ZipArchiveWriterTest.class.php
+++ src/test/php/io/archive/zip/unittest/ZipArchiveWriterTest.class.php
@@ -37,7 +37,7 @@ class ZipArchiveWriterTest extends ZipFileTest {
   #[@test]
   public function adding_a_file() {
     $out= new MemoryOutputStream();
-    
+
     $fixture= ZipFile::create($out);
     $fixture->addFile(new ZipFileEntry('test.txt'))->getOutputStream()->write('File contents');
     $fixture->close();
@@ -48,11 +48,27 @@ class ZipArchiveWriterTest extends ZipFileTest {
   #[@test]
   public function adding_a_dir() {
     $out= new MemoryOutputStream();
-    
+
     $fixture= ZipFile::create($out);
     $fixture->addDir(new ZipDirEntry('test'));
     $fixture->close();
 
     $this->assertEquals(['test/' => null], $this->entriesWithContentIn($out));
+  }
+
+  #[@test]
+  public function adding_files_and_dir() {
+    $out= new MemoryOutputStream();
+
+    $fixture= ZipFile::create($out);
+    $dir= $fixture->addDir(new ZipDirEntry('test/'));
+    $fixture->addFile(new ZipFileEntry($dir, '1.txt'))->getOutputStream()->write('File #1');
+    $fixture->addFile(new ZipFileEntry($dir, '2.txt'))->getOutputStream()->write('File #2');
+    $fixture->close();
+
+    $this->assertEquals(
+      ['test/' => null, 'test/1.txt' => 'File #1', 'test/2.txt' => 'File #2'],
+      $this->entriesWithContentIn($out)
+    );
   }
 }
